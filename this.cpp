2b7a3307626458b35a06f63519356ec9a0e04a8b
sigsevneo <anaghsingh.nitk@gmail.com>
2014-05-20 23:39:32 +0530

    second commit

--- this.cpp
+++ this.cpp
@@ -8,6 +8,8 @@ int n;
 cin>>n;
 //this program will calculate the square root of a number and print output with 2 decimals
 float q;
+
+printf("HI there\n");
 q=sqrt(n);
 cout<<fixed;
 cout<<setprecision(2)<<q<<"\n";
